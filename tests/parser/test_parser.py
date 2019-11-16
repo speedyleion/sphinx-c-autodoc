019dfbfb479a5e40eb74b49c7151280f9e6173e5
Nick <speedyleion@users.noreply.github.com>
2019-11-16 08:04:59 -0800

    re-factor test to be easier to expand

--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -2,23 +2,21 @@
 Test the parsing of C files into the needed pieces.
 """
 import os
+import pytest
 
 from c_docs import parser
 
 SCRIPT_DIR = os.path.dirname(__file__)
-def test_basic_file_parsing():
-    """
-    Test that a simple C file is turned into 2 sections one for the module level
-    comment and one for the function level comment
-    """
-    comments = parser.parse(os.path.join(SCRIPT_DIR, 'assets', 'one_function.c'))
-    assert comments == ["This is a file comment", "This is a function comment"]
+testdata = [('one_function.c', ["This is a file comment", "This is a function comment"]),
+            ('typedef.c', ["This is a file comment", "This is a type comment", "This is a function comment"]),
+           ]
 
-def test_alternate_file_parsing():
+
+@pytest.mark.parametrize('filename, expected', testdata)
+def test_basic_file_parsing(filename, expected):
     """
     Test that a simple C file is turned into 2 sections one for the module level
     comment and one for the function level comment
     """
-    comments = parser.parse(os.path.join(SCRIPT_DIR, 'assets', 'typedef.c'))
-    assert comments == ["This is a file comment", "This is a type comment",
-                        "This is a function comment"]
+    comments = parser.parse(os.path.join(SCRIPT_DIR, 'assets', filename))
+    assert comments == expected
