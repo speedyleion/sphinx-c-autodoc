3968136c7907b7129b90332f79952024e428c7b2
Nick <speedyleion@users.noreply.github.com>
2019-11-16 08:18:49 -0800

    hooking into clang, missing the dll

--- src/c_docs/parser.py
+++ src/c_docs/parser.py
@@ -2,7 +2,33 @@
 Parser of c files
 """
 
+import textwrap
+from clang import cindex
+
 def parse(filename):
     """
     """
-    return ["This is a file comment", "This is a function comment"]
+    cursor = cindex.TranslationUnit.from_source(filename)
+    
+    # HACK Just to get tests passing re-work later
+    comments = []
+    for t in cursor.get_tokens():
+        if t.kind != clang.cindex.TokenKind.Comment:
+            continue
+
+        comments.appned(parse_comment(t.spelling))
+
+    return comments
+
+def parse_comment(comment):
+    """
+    """
+    # Remove leading and trailing blocks, needs to be more logical
+    comment = comment.splitlines[1:-1]
+
+    # Remove any leading '*'s
+    #???
+
+    comment = '\n'.join(comment).strip()
+
+    return textwrap.dedent(comment)
