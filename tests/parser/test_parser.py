a879a04a9c50b0738a8deffc5c2dc685305642f0
Nick <speedyleion@users.noreply.github.com>
2019-11-16 07:29:14 -0800

    Adding an initial test skeleton

---
+++ tests/parser/test_parser.py
@@ -0,0 +1,15 @@
+"""
+Test the parsing of C files into the needed pieces.
+"""
+import os
+
+from c_docs import parser
+
+SCRIPT_DIR = os.path.dirname(__file__)
+def test_basic_file_parsing():
+    """
+    Test that a simple C file is turned into 2 sections one for the module level
+    comment and one for the function level comment
+    """
+    comments = parser.parse(os.path.join(SCRIPT_DIR, assets, 'one_function.c'))
+    assert comments == ["This is a file comment", "This is a function comment"]
