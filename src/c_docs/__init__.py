1a84341a34695dd95bc68a7d1bb57a54ec2d5d37
Nick <speedyleion@users.noreply.github.com>
2019-11-17 09:12:55 -0800

    Have the directive available now, missing functions

--- src/c_docs/__init__.py
+++ src/c_docs/__init__.py
@@ -3,6 +3,7 @@ from sphinx.ext.autodoc import Documenter
 class CFileDocumenter(Documenter):
     """
     """
+    objtype = 'cfile'
     pass
 
 def setup(app):
@@ -10,4 +11,4 @@ def setup(app):
     Setup function for registering this with sphinx
     """
     app.require_sphinx('1.8')
-    app.add_autodocumenter('cfile', CFileDocumenter)
+    app.add_autodocumenter(CFileDocumenter)
