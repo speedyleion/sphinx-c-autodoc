6ab13328dfd9f7b6516747c0b219f37ea72e6c78
Nick <speedyleion@users.noreply.github.com>
2022-12-19 21:13:58 -0800

    Update version numbers in preparation for release 1.1.0 (#44)

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2019-2021, Nick'
 author = u'Nick'
 
 # The short X.Y version
-version = u'1.0'
+version = u'1.1'
 # The full version, including alpha/beta/rc tags
-release = u'1.0.0'
+release = u'1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
