5f8446e7ed7475b926c478ea971b4a655a5db7b3
Nick <speedyleion@users.noreply.github.com>
2023-06-28 18:17:13 -0700

    Bump version for 1.2.2 release

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = u'Nick'
 # The short X.Y version
 version = u'1.2'
 # The full version, including alpha/beta/rc tags
-release = u'1.2.1'
+release = u'1.2.2'
 
 
 # -- General configuration ---------------------------------------------------
